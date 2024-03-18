5490d30c10d4cb12d7d7645ce804d5d7464a6b65
Raj Kumar Arora <Raj Kumar Arora@RajKumarArora-DellPC>
2024-03-18 22:47:11 +0100

    Enable Authorize button in Swagger UI

--- JWT_Demo/Program.cs
+++ JWT_Demo/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,7 +10,32 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+        Name = "Authorization",
+        Description = "Bearer Authorization with JWT Token",
+        Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http
+    });
+    options.AddSecurityRequirement(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            new List<string> ()
+        }
+    });
+});
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).
     AddJwtBearer(options =>
--- README.txt
+++ README.txt
@@ -1,2 +1,8 @@
-ASP.NET Core Web API Authentication and Authorization with JWT (Json Web Token)
-https://www.youtube.com/watch?v=kM1fPt1BcLc
+* ASP.NET Core Web API Authentication and Authorization with JWT (Json Web Token)
+      https://www.youtube.com/watch?v=kM1fPt1BcLc
+* .NET 6 Web API Authentication | Minimal API & Swagger (CRUD)
+      https://www.youtube.com/watch?v=f2IdQqpjR0c&list=PLYrEifPM-Jqz_RFnr9B9j0Hl4fD-WDpX-&index=1
+
+* For details on how to Authenticate user from Swagger,
+  (How to enable Authorize button in Swagger UI), 
+  https://www.youtube.com/watch?v=f2IdQqpjR0c&list=PLYrEifPM-Jqz_RFnr9B9j0Hl4fD-WDpX-&index=1&t=38:28
